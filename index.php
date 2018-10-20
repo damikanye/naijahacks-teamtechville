b7b9f54a927cf4a1a6b137eabc449cf2d07a77b4
Danielflame <40615325+Danielflame@users.noreply.github.com>
2018-10-20 14:11:40 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Course Land Homepage</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <?php include("navbar.php"); ?><br><br>
+    
+    <div id="Myslide" class="carousel slide carousel-fade" data-ride="carousel">
+    	<!--<ol class="carousel-indicators">
+    		<li data-target="Myslide" data-slide-to="0" class="active"></li>
+    		<li data-target="Myslide" data-slide-to="1"></li>
+    		<li data-target="Myslide" data-slide-to="2"></li>
+    	</ol>-->
+    	<div class="carousel-inner" role="list-box">
+    		<div class="item active" style="background-image:linear-gradient(to left bottom, rgba(0,0,154,1), rgba(0,0,0,.1) ), url(img/2.JPG)">
+    			
+    			<div class="carousel-caption">
+    				<h1>Course <span class="carouselheader">Land</span></h1>
+    				<p><span class="carouselheader">...review your courses online</span></p>
+    			</div>
+    		</div>
+    		<div class="item" style="background-image:linear-gradient(to left bottom, rgba(0,0,154,1), rgba(0,0,0,.1) ), url(img/2.JPG)">
+    			
+    			<div class="carousel-caption">
+    				<h1>Course <span class="carouselheader">Land</span></h1>
+    				<p><span class="carouselheader">...review your courses online</span></p>
+    			</div>
+    		</div>
+    		<div class="item " style="background-image:linear-gradient(to left bottom, rgba(0,0,154,1), rgba(0,0,0,.1) ), url(img/2.JPG)">
+    			
+    			<div class="carousel-caption">
+    				<h1>Course <span class="carouselheader">Land</span></h1>
+    				<p><span class="carouselheader">...review your courses online</span></p>.
+    			</div>
+    		</div>
+    	</div>
+    	
+
+    </div><!-- end slider-->
+    <br><!-- end ofnav-->
+	<div class="container">
+		<center>
+			<span class="carouselheader"><h1>Our Courses</h1></span>
+      <hr style="border-color:orange; width:70%; border:3px solid orange; border-radius:10px;"> 
+		</center>
+		<div class="row">
+			<div class="col-md-6 col-lg-3 col-sm-6">
+				<center><img src="img/berg.jpg"></center>
+				<center><h1><span class="carouselheader">Science</span></h1></center>
+				<p>
+					Web Store Design Services are websites that we build to your specifications, including eCommerce (shopping cart) functionality. There are a variety of Web Store plans to choose from that allow you to dictate many aspects of your site, such as layout, color schemes, and organization <span><button><a href="science.html">Take Course</a></button></span>
+				</p>
+			</div>
+			<div class="col-md-6 col-lg-3 col-sm-6">
+				<center><img src="img/berg.jpg"></center>
+				<center><h1><span class="carouselheader">Technology</span></h1></center>
+				<p>
+					Web Store Design Services are websites that we build to your specifications, including eCommerce (shopping cart) functionality. There are a variety of Web Store plans to choose from that allow you to dictate many aspects of your site, such as layout, color schemes, and organization <span><button><a href="technology.html">Take Course</a></button></span>
+				</p>
+			</div>
+			<div class="col-md-6 col-lg-3 col-sm-6">
+				<center><img src="img/berg.jpg"></center>
+				<center><h1><span class="carouselheader">Engineering</span></h1></center>
+				<p>
+					Web Store Design Services are websites that we build to your specifications, including eCommerce (shopping cart) functionality. There are a variety of Web Store plans to choose from that allow you to dictate many aspects of your site, such as layout, color schemes, and organization <span><button><a href="engineering.html">Take Course</a></button></span>
+				</p>
+			</div>
+			<div class="col-md-6 col-lg-3 col-sm-6">
+				<center><img src="img/berg.jpg"></center>
+				<center><h1><span class="carouselheader">Mathematics</span></h1></center>
+				<p>
+					Web Store Design Services are websites that we build to your specifications, including eCommerce (shopping cart) functionality. There are a variety of Web Store plans to choose from that allow you to dictate many aspects of your site, such as layout, color schemes, and organization <span><button><a href="mathematics.html">Take Course</a></button></span>
+				</p>
+			</div>
+		</div>
+	</div><br><!-- end of container-->
+	<div class="container">
+		<center><h1><span class="carouselheader">Our Team</span></h1></center>
+		<hr style="border-color:orange; width:70%; border:3px solid orange; border-radius:10px;">
+       <div class="row">
+       	<div class="col-md-4 col-sm-4">
+       		<center><img src="img/berg.jpg" class="imgbd"></center>
+       		<center><h1>Helen Ogbogu</h1></center>
+       		<center><p>Founder Tehchville/phyton dev</p></center>
+       	</div>
+       	<div class="col-md-4 col-sm-4">
+       		<center><img src="img/berg.jpg" class="imgbd"></center>
+       		<center><h1>Helen Ogbogu</h1></center>
+       		<center><p>Founder Tehchville/phyton dev</p></center>
+       	</div>
+       	<div class="col-md-4 col-sm-4">
+       		<center><img src="img/berg.jpg" class="imgbd"></center>
+       		<center><h1>Helen Ogbogu</h1></center>
+       		<center><p>Founder Tehchville/phyton dev</p></center>
+       	</div>
+       </div>	
+
+	</div>
+   
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,39 @@
+<style type="text/css">
+  .navbar{
+      margin-bottom:0;
+      border-radius:0;
+      /*background-image:linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.4));*/
+      
+      padding:1% 0;
+      font-size:1.2em;
+      border:0;
+
+      
+}
+.navbar-brand img{
+   margin:-5px 0 0 0;
+}
+#navbar{
+  height:100px;
+}
+
+</style>
+<nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: orange ;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href=""><img src="img/courseland.png" width="200px" height="60px" style="margin-top: -20px;"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="MyNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="contact.html">Contact us</a></li>
+        </ul>
+      </div>
+  </div>
+  </nav>
